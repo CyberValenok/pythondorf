42437f81b9e93a6b741f9c7ca160ec7e3d1693b3
PussyMan <alendorf.alex@yandex.ru>
2018-12-27 05:53:22 +0400

    Fix authorization

--- src/ums/__init__.py
+++ src/ums/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
+
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
--- src/ums/urls.py
+++ src/ums/urls.py
@@ -2,6 +2,9 @@ from ums import app
 from ums import db
 from ums.models import User
 from flask import abort, request, render_template, redirect, url_for, session, flash
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt(app)
 
 @app.route('/')
 def redirhome():
@@ -14,31 +17,39 @@ def home():
 @app.route('/login', methods=['GET','POST'])
 def login():
     if request.method == 'POST':
-        UsNa=request.form['login']
-        Pass=request.form['password']
-        if not (UsNa and Pass):
+        username=request.form['login']
+        password=request.form['password']
+        if not (username and password):
             flash("Field(s) is empy")
             return redirect(url_for('login'))
         else:
-            UsNa=UsNa.strip(' ')
-            Pass=Pass.strip(' ')
-        user = User.query.filter_by(UsNa=username).first()
-        if UsNa and check_pass(user.password, Pass):
-            session[UsNa] = True
-            return redirect(url_for('us_strt', UsNa=username))
+            username=username.strip(' ')
+            password=password.strip(' ')
+
+        user = User.query.filter_by(username=username).first()
+        if username and bcrypt.check_password_hash(user.password, password):
+            session[username] = True
+            return redirect(url_for('userpage', username=username))
         else:
-            flash("PRIVET POSHEL NAHUI, Account does not exist!")
-    return render_template('log.html')
-@app.route('/user/<UsNa>/')
-def us_strt():
-    if not session.get(UsNa):
+            flash("PRIVET POSHEL NAHUI, account does not exist!")
+    return render_template('login.html')
+
+@app.route('/home/<username>/')
+def userpage(username):
+    if not session.get(username):
         abort(401)
-    return render_template('userpage.html')
+    return render_template('userpage.html', username=username)
+
 @app.route('/signup', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def users():
     if request.method== 'POST':
-        reg = User(request.form['login'].strip(' '), request.form['password'].strip(' '))
+        username=request.form['login'].strip(' ')
+        password=request.form['password'].strip(' ')
+
+        pass_hash=bcrypt.generate_password_hash(password)
+
+        user=User(username=username, password=pass_hash)
         db.create_all()
-        db.session.add(reg)
+        db.session.add(user)
         db.session.commit()
-    return render_template('reg.html')
+    return render_template('registration.html')
