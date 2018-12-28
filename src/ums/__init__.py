6a18db837e749f93a9e81c7d8aecc503ab43f7ae
PussyMan <alendorf.alex@yandex.ru>
2018-12-28 17:24:57 +0400

    fix sign in/up

--- src/ums/__init__.py
+++ src/ums/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
-
+bcrypt = Bcrypt(app)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 
--- src/ums/urls.py
+++ src/ums/urls.py
@@ -2,9 +2,8 @@ from ums import app
 from ums import db
 from ums.models import User
 from flask import abort, request, render_template, redirect, url_for, session, flash
-from flask_bcrypt import Bcrypt
-
-bcrypt = Bcrypt(app)
+from ums import bcrypt
+from sqlalchemy.exc import IntegrityError
 
 @app.route('/')
 def redirhome():
@@ -20,18 +19,20 @@ def login():
         username=request.form['login']
         password=request.form['password']
         if not (username and password):
-            flash("Field(s) is empy")
-            return redirect(url_for('login'))
+            flash('Field(s) is empy!')
+            return redirect(url_for("login"))
         else:
             username=username.strip(' ')
             password=password.strip(' ')
 
         user = User.query.filter_by(username=username).first()
-        if username and bcrypt.check_password_hash(user.password, password):
+
+        if user and bcrypt.check_password_hash(user.password, password):
             session[username] = True
             return redirect(url_for('userpage', username=username))
         else:
-            flash("PRIVET POSHEL NAHUI, account does not exist!")
+            flash('PRIVET POSHEL NAHUI, account does not exist!')
+            return redirect(url_for('login'))
     return render_template('login.html')
 
 @app.route('/home/<username>/')
@@ -40,16 +41,29 @@ def userpage(username):
         abort(401)
     return render_template('userpage.html', username=username)
 
+#@app.route('/logout/<username>')
+#def logout(username):
+#    session.pop(username, none)
+
+
 @app.route('/signup', methods=['GET', 'POST', 'PUT', 'DELETE'])
 def users():
     if request.method== 'POST':
         username=request.form['login'].strip(' ')
         password=request.form['password'].strip(' ')
+        if not (username and password):
+            flash('Field(s) is empty!')
 
         pass_hash=bcrypt.generate_password_hash(password)
 
         user=User(username=username, password=pass_hash)
         db.create_all()
         db.session.add(user)
-        db.session.commit()
+        try:
+            db.session.commit()
+        except IntegrityError:
+            flash("Toha, ne obmanyvai! Account {username} not been created".format(username=username))
+            return redirect(url_for("users"))
+        flash("KRASAVA!!!!!!!!, zahodi")
+        return redirect(url_for("login"))
     return render_template('registration.html')
